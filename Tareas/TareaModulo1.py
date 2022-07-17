099ac9fc43048b177dfd804347516b146075a33a
Fabian <fasaldarriaga.phyc1@bootcamp.ucamp.io>
2022-07-17 18:34:05 -0500

    Tarea Modulo 1

---
+++ Tareas/TareaModulo1.py
@@ -0,0 +1,22 @@
+# Este programa consiste en un formulario en el cual se le pedira al usuario que ingrese informacion personal
+# la cual sera utilizada para calcular su indice de masa corporal (IMC).
+# Una vez obtenidos los datos, se le mostrara los resultados del formulario al usuario incluido su IMC.
+
+print('A continuacion te pediremos algunos datos personales')
+nombre = input('Por favor introduce tu nombre: ')
+apellido1 = input('Por favor introduce tu apellido paterno: ')
+apellido2 = input('Por favor introduce tu apellido materno: ')
+edad = int(input('Por favor introduce tu edad: '))
+peso = float(input('Por favor introduce tu peso (kilos): '))
+estatura = float(input('Por favor introduce tu estatura (metros): '))
+print('Tus datos personales son:')
+print('Tu nombre completo es: ' + nombre + ' ' + apellido1 + ' ' + apellido2)
+print('Tu edad es: ' + str(edad) + ' años')
+print('Tu peso es: ' + str(peso) + ' kilos (kg)')
+print('Tu estatura es: ' + str(estatura) + ' metros (m)')
+
+print('Ahora calcularemos tu masa corporal')
+
+print(f'Tu indice de masa corporal es: {peso/estatura**2} kg/m2')
+
+print('Muchas gracias por utilizar este programa, saludos!')
