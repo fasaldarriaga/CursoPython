366e050a4ad9579f570fe415086efd224e72218a
Fabian <fasaldarriaga.phyc1@bootcamp.ucamp.io>
2022-07-17 16:48:33 -0500

    Add archivos de practicas

---
+++ cadenas.py
@@ -0,0 +1,31 @@
+
+#Se pueden utilizar comillas triples para que el texto se muestre tal cual y como la hemos escrito 
+#El caracter \ hace que se muestre los datos inmediatamente siguientes
+
+# print("""
+# Funcionamiento de \
+# programas: (opciones)
+# -1 para acceder a opciones
+# -2 para salir
+# """)
+
+#############################################################################
+
+#Subscripsting e indexado 
+#no se le puede pedir un valor que exceda el limite de caracteres de la cadena
+# ya que la posicion no existe, ej: print(texto[7])
+
+# texto = 'Python'
+
+# print(texto[0] )
+# print(texto[-1])
+
+#Nota: No se pueden modificar las cadenas una vez definidas
+
+###########################################################################
+
+#Slicing o Subscrining
+
+texto = 'Python'
+print(texto[0:3])
+print(texto[0:-3])
