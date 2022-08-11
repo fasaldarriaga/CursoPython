f01b62d5e41956fc25e37f8aee5f4def0e0deb6d
Fabian <fasaldarriaga.phyc1@bootcamp.ucamp.io>
2022-08-11 17:22:19 -0500

    Correccion proyecto modulo 1

--- Tareas/TareaModulo1.py
+++ Tareas/TareaModulo1.py
@@ -6,9 +6,19 @@ print('A continuacion te pediremos algunos datos personales')
 nombre = input('Por favor introduce tu nombre: ')
 apellido1 = input('Por favor introduce tu apellido paterno: ')
 apellido2 = input('Por favor introduce tu apellido materno: ')
-edad = int(input('Por favor introduce tu edad: '))
-peso = float(input('Por favor introduce tu peso (kilos): '))
-estatura = float(input('Por favor introduce tu estatura (metros): '))
+try:
+    edad = int(input('Por favor introduce tu edad: '))
+except:
+    print('Debes ingresar un valor numerico entero')
+try:
+    peso = float(input('Por favor introduce tu peso (kilos): '))
+except:
+    print('Debes ingresar un valor numerico')
+try:
+    estatura = float(input('Por favor introduce tu estatura (metros): '))
+except:
+    print('Debes ingresar un valor numerico')
+
 print('Tus datos personales son:')
 print('Tu nombre completo es: ' + nombre + ' ' + apellido1 + ' ' + apellido2)
 print('Tu edad es: ' + str(edad) + ' años')
@@ -17,6 +27,6 @@ print('Tu estatura es: ' + str(estatura) + ' metros (m)')
 
 print('Ahora calcularemos tu masa corporal')
 
-print(f'Tu indice de masa corporal es: {peso/estatura**2} kg/m2')
+print(f'Tu indice de masa corporal es: ' {peso/estatura**2} 'kg/m2')
 
 print('Muchas gracias por utilizar este programa, saludos!')
